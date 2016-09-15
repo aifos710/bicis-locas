12c22a3269ad08b043169af8cc5cd2ab33fa061a
aifos710 <sofia.m.caballero@gmail.com>
2016-09-15 11:47:08 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	/* validacion de campos obligatorios */
+	var x = document.getElementById("name").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+    var x = document.getElementById("lastname").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+    var x = document.getElementById("input-email").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+    var x = document.getElementById("input-password").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+    /* validacion de email*/
+    var email = document.getElementById("input-email").value;
+	if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
+  	return false;
+	};
+	/* validacion de contraseña*/
+	var pass = document.getElementById("input-password").value;
+    if (pass.length <= 6 || pass.length == 0 || pass == "123456" || pass == "98754" || pass == "password" ) {
+        alert("Contraseña no Valida");
+    return false; 
+     };
+	
+    /* validacion de mayuscula */
+    /* validacion de bicis */
 }
